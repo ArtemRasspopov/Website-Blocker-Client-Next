48f8aa0f130e6f47c47fcbdf49cc4c372e8a032b
Artem Rasspopov <Artem199833@gmail.com>
2023-11-17 11:20:04 +0300

    added uiKit

---
+++ src/shared/ui/ui-button.tsx
@@ -0,0 +1,24 @@
+import clsx from "clsx";
+import { ButtonHTMLAttributes } from "react";
+
+type UiButtonVariant = "primery" | "secondary" | "outlined";
+export type UiButtonProps = {
+  variant: UiButtonVariant;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
+
+export function UiButton({ className, variant, ...props }: UiButtonProps) {
+  return (
+    <button
+      {...props}
+      className={clsx(
+        className,
+        "px-4 h-10 rounded cursor-pointer flex gap-2 items-center justify-center",
+        {
+          primery: "text-white bg-teal-500 shadow shadow-teal-500/30 hover:bg-teal-600 disabled:opacity-50",
+          secondary: "text-white bg-rose-500 shadow shadow-teal-500/30 hover:bg-rose-600 disabled:opacity-50",
+          outlined: "border border-slate-300 hover:border-slate-500 disabled:opacity-50",
+        }[variant]
+      )}
+    />
+  );
+}
